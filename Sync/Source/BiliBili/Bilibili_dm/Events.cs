428c175e35285b54d62403b9cdddd3f02df4e6ef
Deliay <admin@remiliascarlet.com>
2017-01-08 01:04:38 +0800

    Bilibili_dm import

---
+++ Sync/Source/BiliBili/Bilibili_dm/Events.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BilibiliDM_PluginFramework
+{
+    public delegate void DisconnectEvt(object sender, DisconnectEvtArgs e);
+
+    public delegate void ReceivedDanmakuEvt(object sender, ReceivedDanmakuArgs e);
+
+    public delegate void ReceivedRoomCountEvt(object sender, ReceivedRoomCountArgs e);
+    public delegate void ConnectedEvt(object sender, ConnectedEvtArgs e);
+
+    public class ReceivedRoomCountArgs
+    {
+        public uint UserCount;
+    }
+
+    public class DisconnectEvtArgs
+    {
+        public Exception Error;
+    }
+
+    public class ReceivedDanmakuArgs
+    {
+        public DanmakuModel Danmaku;
+    }
+    public class ConnectedEvtArgs
+    {
+        public int roomid;
+    }
+}
---
+++ Sync/Source/BiliBili/Bilibili_dm/utils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Net.Sockets;
+
+namespace BiliDMLib
+{
+    public static class utils
+    {
+        public static byte[] ToBE(this byte[] b)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                return b.Reverse().ToArray();
+            }
+            else
+            {
+                return b;
+            }
+        }
+        public static void ReadB(this NetworkStream stream, byte[] buffer, int offset, int count)
+        {
+            if (offset + count > buffer.Length)
+                throw new ArgumentException();
+            int read = 0;
+            while (read < count)
+            {
+                var available = stream.Read(buffer, offset, count - read);
+                if (available == 0)
+                {
+                    throw new ObjectDisposedException(null);
+                }
+                //                if (available != count)
+                //                {
+                //                    throw new NotSupportedException();
+                //                }
+                read += available;
+                offset += available;
+
+            }
+
+        }
+    }
+}
