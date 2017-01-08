573e84b573b175639436e003d424e51c6b57e44a
Deliay <admin@remiliascarlet.com>
2017-01-08 16:53:40 +0800

    Release v1.2.0.3

--- Sync/Properties/AssemblyInfo.cs
+++ Sync/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RemiliaNetwork")]
 [assembly: AssemblyProduct("LiveSyncIRC")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright Free 2017")]
 [assembly: AssemblyTrademark("RemiliaScarlet.com")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.3")]
-[assembly: AssemblyFileVersion("0.1.0.3")]
+[assembly: AssemblyVersion("1.2.0.3")]
+[assembly: AssemblyFileVersion("1.2.0.3")]
