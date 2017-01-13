c1eb0147e4a90c505d526b364873d3d15ef3e7cf
Deliay <admin@remiliascarlet.com>
2017-01-13 14:27:07 +0800

    PluginConfiguration Tools up

---
+++ Sync/Tools/IConfigurable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.Tools
+{
+    public interface IConfigurable
+    {
+        /// <summary>
+        /// 在系统读取配置文件时调用
+        /// </summary>
+        void onConfigurationLoad();
+        /// <summary>
+        /// 在系统保存配置文件时调用
+        /// </summary>
+        void onConfigurationSave();
+    }
+}
