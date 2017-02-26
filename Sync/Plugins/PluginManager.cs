023d3b15725e66ca285eb85fbb322c8adcfdf6d1
Deliay <admin@remiliascarlet.com>
2017-02-26 14:58:27 +0800

    PluginManager bugs fix 1.Skip already loaded Assembly when load Plugins 2.Skip Plugin itself(Assignable form Plugin) 3.Set pluginList to null when Dispose

--- Sync/Plugins/PluginManager.cs
+++ Sync/Plugins/PluginManager.cs
@@ -88,7 +88,7 @@ namespace Sync.Plugins
 
             pluginList = new List<Plugin>();
             asmList = new List<Assembly>();
-
+            asmList.AddRange(AppDomain.CurrentDomain.GetAssemblies());
             if (!Directory.Exists(path)) return 0;
             Directory.SetCurrentDirectory(path);
 
@@ -97,6 +97,8 @@ namespace Sync.Plugins
             {
                 try
                 {
+                    if (asmList.Where(a => a.Location == file).Count() != 0)
+                        continue;
                     Assembly asm = Assembly.LoadFrom(file);
                     asmList.Add(asm);
                 }
@@ -116,7 +118,8 @@ namespace Sync.Plugins
                         Type it = asm.GetType(t.FullName);
                         if (it == null ||
                             !it.IsClass || !it.IsPublic ||
-                            !typeof(Plugin).IsAssignableFrom(it))
+                            !typeof(Plugin).IsAssignableFrom(it) ||
+                            typeof(Plugin) == it)
                             continue;
 
                         object pluginTest = asm.CreateInstance(it.FullName);
@@ -142,7 +145,7 @@ namespace Sync.Plugins
             {
                 p.Dispose();
             }
-
+            pluginList = null;
         }
     }
 }
