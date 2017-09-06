ebe8348562160c2b89d9962bddc731956eb492ce
Deliay <admin@remiliascarlet.com>
2017-09-07 01:13:53 +0800

    New IRC Client directly sync Sync->OSU (not complete

---
+++ DefaultPlugin/Clients/DirectOSUIRCBot.cs
@@ -0,0 +1,47 @@
+﻿using Sync.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sync.MessageFilter;
+
+namespace DefaultPlugin.Clients
+{
+    class DirectOSUIRCBot : DefaultClient
+    {
+        public DirectOSUIRCBot() : base("Deliay", "DirectOsuIRCBot")
+        {
+        }
+
+        public override void Restart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SendMessage(IMessageBase message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StartWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void StopWork()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SwitchOtherClient()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SwitchThisClient()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
