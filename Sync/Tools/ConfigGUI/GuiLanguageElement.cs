6132232bf016d1462daf06bd0388a0c8606feb73
KedamaOvO <moe@mao-yu.net>
2018-04-20 17:08:49 +0800

    rename ConfigI18n to GUILanguageElement

---
+++ Sync/Tools/ConfigGUI/GuiLanguageElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.Tools.ConfigGUI
+{
+    public class GuiLanguageElement : LanguageElement
+    {
+        public GuiLanguageElement(string defaultVal) : base(defaultVal)
+        {
+        }
+
+        public static implicit operator GuiLanguageElement(string val)
+        {
+            return new GuiLanguageElement(val);
+        }
+
+        public static implicit operator string(GuiLanguageElement element)
+        {
+            return element.ToString();
+        }
+    }
+}
