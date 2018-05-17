f7a4c2fed0f540a72d0319dbdef889d402ad7953
Mikira Sora <mikirasora0409@126.com>
2018-05-17 23:19:06 +0800

    Migrate part of DefaultPlugin commands to InternalPlugin

--- Sync/Plugins/BuildInPlugin/InternalPlugin.cs
+++ Sync/Plugins/BuildInPlugin/InternalPlugin.cs
@@ -3,16 +3,20 @@ using Sync.Tools;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Reflection;
+using static Sync.Tools.DefaultI18n;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Sync.Source;
+using Sync.MessageFilter;
 
 namespace Sync.Plugins.BuildInPlugin
 {
@@ -70,12 +74,309 @@ namespace Sync.Plugins.BuildInPlugin
             this.EventBus.BindEvent<PluginEvents.InitCommandEvent>(p => {
                 Func<string, CommandDelegate, string, bool> addCmd = p.Commands.Dispatch.bind;
                 addCmd("plugins", Plugins, "Install & Update Plugins online, type 'plugins' to get help.");
+                BindCommondCommand(p.Commands.Dispatch);
             });
 
             Updater.update = this;
         }
 
         #region Command Execute
+
+        #region Commond Command
+
+        private void BindCommondCommand(CommandDispatch dispatch)
+        {
+            dispatch.bind("restart", restart, LANG_COMMANDS_RESTART);
+            dispatch.bind("stop", stop, LANG_COMMANDS_STOP);
+            dispatch.bind("start", start, LANG_COMMANDS_START);
+            dispatch.bind("status", status, LANG_COMMANDS_STATUS);
+            dispatch.bind("sourcemsg", sourcemsg, LANG_COMMANDS_SOURCEMSG);
+            dispatch.bind("clientmsg", clientmsg, LANG_COMMANDS_CLIENTMSG);
+
+            dispatch.bind("clientusermsg", chatuser, LANG_COMMANDS_CLIENTUSERMSG);
+            dispatch.bind("disable", disable, LANG_COMMANDS_DISABLE);
+            dispatch.bind("client", switchclient, LANG_COMMANDS_SWITCH_CLIENT);
+            dispatch.bind("sourcelogin", sourcelogin, LANG_COMMANDS_SOURCELOGIN);
+
+            dispatch.bind("clear", clear, LANG_COMMANDS_CLEAR);
+            
+            dispatch.bind("help", help, LANG_COMMANDS_HELP);
+            dispatch.bind("listlang", languages, LANG_COMMANDS_LISTLANG);
+
+            dispatch.bind("lang", language, LANG_COMMANDS_LANG);
+            dispatch.bind("msgmgr", msgmgr, LANG_COMMANDS_MSGMGR);
+            dispatch.bind("sources", listsource, LANG_COMMANDS_SOURCES);
+            dispatch.bind("filters", filters, LANG_COMMANDS_FILTERS);
+            
+        }
+
+        public bool listsource(Arguments arg)
+        {
+            foreach (SourceBase src in SyncHost.Instance.Sources.SourceList)
+            {
+                IO.CurrentIO.WriteColor("", ConsoleColor.Gray, false);
+                IO.CurrentIO.WriteColor(LANG_COMMANDS_SOURCES_NAME, ConsoleColor.Cyan, false, false);
+                IO.CurrentIO.WriteColor(src.Name.PadRight(18), ConsoleColor.White, false, false);
+                IO.CurrentIO.WriteColor(LANG_COMMANDS_SOURCES_AUTHOR, ConsoleColor.DarkCyan, false, false);
+                IO.CurrentIO.WriteColor(src.Author, ConsoleColor.White, true, false);
+            }
+            return true;
+        }
+
+        private bool switchclient(Arguments arg)
+        {
+            if (arg.Count == 0)
+            {
+                foreach (var item in SyncHost.Instance.Clients.Clients)
+                {
+                    IO.CurrentIO.WriteColor("", ConsoleColor.Gray, false);
+                    IO.CurrentIO.WriteColor(LANG_COMMANDS_CLIENT_NAME, ConsoleColor.Cyan, false, false);
+                    IO.CurrentIO.WriteColor(item.ClientName.PadRight(18), ConsoleColor.White, false, false);
+                    IO.CurrentIO.WriteColor(LANG_COMMANDS_CLIENT_AUTHOR, ConsoleColor.DarkCyan, false, false);
+                    IO.CurrentIO.WriteColor(item.Author, ConsoleColor.White, true, false);
+                }
+
+                IO.CurrentIO.WriteColor(string.Format(LANG_COMMANDS_CURRENT, SyncHost.Instance.ClientWrapper.Client.ClientName), ConsoleColor.Green);
+            }
+            else
+            {
+                if (SyncHost.Instance.Clients.Clients.FirstOrDefault(p => p.ClientName == arg[0]) == null) return false;
+                Configuration.Instance.Client = arg[0];
+                SyncHost.Instance.ClientWrapper.ResetClient();
+            }
+            return true;
+        }
+        
+        private bool disable(Arguments arg)
+        {
+            foreach (var item in Sync.SyncHost.Instance.EnumPluings())
+            {
+                if (item.Name == arg[0])
+                {
+                    item.OnDisable();
+                }
+            }
+            return true;
+        }
+
+        private bool sourcelogin(Arguments arg)
+        {
+            if (SyncHost.Instance.SourceWrapper.Sendable)
+            {
+                if (arg.Count == 0) SyncHost.Instance.SourceWrapper.SendableSource.Login("", "");
+                else if (arg.Count == 1) SyncHost.Instance.SourceWrapper.SendableSource.Login(arg[0], "");
+                else if (arg.Count == 2) SyncHost.Instance.SourceWrapper.SendableSource.Login(arg[0], arg[1]);
+            }
+
+            return true;
+        }
+
+        public bool clientmsg(Arguments arg)
+        {
+            if (arg.Count == 0 || (SyncHost.Instance.SourceWrapper.SendableSource != null && SyncHost.Instance.SourceWrapper.SendableSource.SendStatus == false))
+            {
+                IO.CurrentIO.Write(LANG_COMMANDS_CHAT_IRC_NOTCONNECT);
+                return true;
+            }
+
+            SyncHost.Instance.Messages.RaiseMessage<ISourceClient>(new IRCMessage("Console", string.Join(" ", arg)));
+            return true;
+
+        }
+
+        public bool chatuser(Arguments arg)
+        {
+            if (arg.Count < 1 || (SyncHost.Instance.SourceWrapper.SendableSource != null && SyncHost.Instance.SourceWrapper.SendableSource.SendStatus == false))
+            {
+                IO.CurrentIO.Write(LANG_COMMANDS_CHAT_IRC_NOTCONNECT);
+            }
+            string message = "";
+            for (int i = 1; i < arg.Count; i++)
+                message += arg[i] + " ";
+            SyncHost.Instance.Messages.RaiseMessage<ISourceClient>(new IRCMessage(arg[0].Trim(), message));
+            return true;
+
+        }
+
+        public bool sourcemsg(Arguments arg)
+        {
+            if (SyncHost.Instance.SourceWrapper.Sendable)
+            {
+                if (SyncHost.Instance.SourceWrapper.SendableSource.SendStatus)
+                {
+                    SyncHost.Instance.SourceWrapper.SendableSource.Send(new IRCMessage(string.Empty, string.Join("", arg)));
+                    return true;
+                }
+                else
+                {
+                    IO.CurrentIO.Write(LANG_COMMANDS_DANMAKU_REQUIRE_LOGIN);
+                }
+            }
+            else
+            {
+                IO.CurrentIO.Write(LANG_COMMANDS_DANMAKU_NOT_SUPPORT);
+            }
+            return true;
+        }
+
+        public bool start(Arguments arg)
+        {
+            if (SyncHost.Instance.SourceWrapper.Source.Status == SourceStatus.CONNECTED_WORKING)
+            {
+                IO.CurrentIO.Write(LANG_COMMANDS_START_ALREADY_RUN);
+                return true;
+            }
+            SyncHost.Instance.ClientWrapper?.Client?.StartWork();
+            SyncHost.Instance.SourceWrapper?.Source?.Connect();
+            return true;
+        }
+
+        public bool stop(Arguments arg)
+        {
+            if (SyncHost.Instance.ClientWrapper.Client != null) SyncHost.Instance.ClientWrapper.Client?.StopWork();
+            if (SyncHost.Instance.SourceWrapper.Source != null) SyncHost.Instance.SourceWrapper.Source?.Disconnect();
+            return true;
+        }
+
+        public bool status(Arguments arg)
+        {
+            IO.CurrentIO.WriteStatus();
+            return true;
+        }
+
+        public bool language(Arguments arg)
+        {
+            if (arg.Count == 0)
+            {
+                CultureInfo info = CultureInfo.GetCultureInfo(I18n.Instance.CurrentLanguage);
+                IO.CurrentIO.WriteColor(string.Format("Current culture: {0:S}\t{1:S}", info.Name, info.NativeName), ConsoleColor.Yellow);
+                return true;
+            }
+            else if (arg.Count == 1)
+            {
+                try
+                {
+                    CultureInfo info = CultureInfo.GetCultureInfo(arg[0]);
+                    Configuration.Instance.Language = arg[0];
+                    IO.CurrentIO.WriteColor(string.Format("Success switch to {1:S}({0:S})", arg[0], info.NativeName), ConsoleColor.Green);
+                    return true;
+                }
+                catch (CultureNotFoundException)
+                {
+                    IO.CurrentIO.WriteColor("Culture not found! Please specify other culture name.", ConsoleColor.Red);
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public bool clear(Arguments arg)
+        {
+            IO.CurrentIO.Clear();
+            IO.CurrentIO.WriteWelcome();
+            return true;
+        }
+
+        public bool help(Arguments arg)
+        {
+            IO.CurrentIO.WriteHelp();
+            return true;
+        }
+
+        public bool msgmgr(Arguments arg)
+        {
+            int value = 0;
+
+            if (arg.Count == 0)
+                IO.CurrentIO.WriteColor(LANG_COMMANDS_ARGUMENT_WRONG, ConsoleColor.Red);
+            else
+            {
+                switch (arg[0].Trim())
+                {
+                    case "--help":
+                        IO.CurrentIO.WriteColor(LANG_COMMANDS_MSGMGR_HELP, ConsoleColor.Yellow);
+                        break;
+                    case "--status":
+                        IO.CurrentIO.WriteColor(String.Format(LANG_COMMANDS_MSGMGR_STATUS, (string)(MessageManager.IsLimit ? LANG_COMMANDS_MSGMGR_LIMIT : LANG_COMMANDS_MSGMGR_FREE), MessageManager.CurrentQueueCount, MessageManager.LimitLevel, MessageManager.RecoverTime, MessageManager.Option.ToString()), ConsoleColor.Yellow);
+                        break;
+                    case "--limit":
+                        if (arg.Count == 2 && Int32.TryParse(arg[1].Trim(), out value))
+                        {
+                            MessageManager.LimitLevel = value;
+                            IO.CurrentIO.WriteColor(string.Format(LANG_COMMANDS_MSGMGR_LIMIT_SPEED_SET, MessageManager.LimitLevel), ConsoleColor.Yellow);
+                        }
+                        else
+                            IO.CurrentIO.WriteColor(LANG_COMMANDS_ARGUMENT_WRONG, ConsoleColor.Red);
+                        break;
+
+                    case "--option":
+                        if (arg.Count == 2)
+                        {
+                            switch (arg[1].Trim().ToLower())
+                            {
+                                case "auto":
+                                    MessageManager.Option = MessageManager.PeekOption.Auto;
+                                    break;
+                                case "force_all":
+                                    MessageManager.Option = MessageManager.PeekOption.Force_All;
+                                    break;
+                                case "force_limit":
+                                    MessageManager.Option = MessageManager.PeekOption.Only_Send_Command;
+                                    break;
+                            }
+                            IO.CurrentIO.WriteColor(string.Format(LANG_COMMANDS_MSGMGR_LIMIT_STYPE_SET, MessageManager.Option.ToString()), ConsoleColor.Yellow);
+                        }
+                        else
+                            IO.CurrentIO.WriteColor(LANG_COMMANDS_ARGUMENT_WRONG, ConsoleColor.Red);
+                        break;
+                }
+            }
+            return true;
+        }
+
+        public bool languages(Arguments arg)
+        {
+            if (arg.Count > 0 && arg[0] == "--all")
+            {
+                foreach (var item in CultureInfo.GetCultures(CultureTypes.NeutralCultures))
+                {
+                    IO.CurrentIO.WriteColor(string.Format("CultureName: {0:S}\t{1:S}", item.Name, item.NativeName), ConsoleColor.Yellow);
+                }
+            }
+            else
+            {
+                foreach (var item in System.IO.Directory.EnumerateDirectories(I18n.Instance.LangFolder))
+                {
+                    string name = item.Substring(item.LastIndexOf('\\') + 1);
+                    IO.CurrentIO.WriteColor(string.Format("CultureName: {0:S}\t{1:S}", name, CultureInfo.GetCultureInfo(name).NativeName), ConsoleColor.Yellow);
+                }
+            }
+            return true;
+        }
+
+        private bool restart(Arguments arg)
+        {
+            Process.Start(Assembly.GetEntryAssembly().Location);
+            Environment.Exit(0);
+            return true;
+        }
+
+        private bool filters(Arguments arg)
+        {
+            foreach (var item in SyncHost.Instance.Filters.GetFiltersEnum())
+            {
+                IO.CurrentIO.WriteColor("", ConsoleColor.Gray, false);
+                IO.CurrentIO.WriteColor(LANG_COMMANDS_FILTERS_ITEM, ConsoleColor.Cyan, false, false);
+                IO.CurrentIO.WriteColor(item.Key.Name.PadRight(22), ConsoleColor.White, false, false);
+                IO.CurrentIO.WriteColor(LANG_COMMANDS_FILTERS_OBJ, ConsoleColor.DarkCyan, false, false);
+                IO.CurrentIO.WriteColor(item.Value.GetType().Name, ConsoleColor.White, true, false);
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region Update Command
 
         private bool Plugins(Arguments arg)
         {
@@ -254,8 +555,10 @@ namespace Sync.Plugins.BuildInPlugin
 
         #endregion
 
+        #endregion
+
         #region Update Tool
-        
+
         private void RequireRestart(string msg)
         {
             IO.CurrentIO.WriteColor($"{msg}? (Y/N):", ConsoleColor.Green, false);
--- Sync/Tools/I18n.cs
+++ Sync/Tools/I18n.cs
@@ -2,10 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sync.Tools
 {
@@ -83,8 +80,46 @@ namespace Sync.Tools
         public static LanguageElement LANG_Plugin_Cycle_Reference = "发现插件之间的循环引用关系，插件 {0:S} 将不会按照开发者指定的依赖关系进行加载";
 
         public static LanguageElement LANG_Instance_Exist = "只能存在一个Sync进程";
-    }
 
+        //from default plugin
+        public static LanguageElement LANG_COMMANDS_LOGIN = "login <user> [pass] 登录到目标弹幕网站，启动弹幕发送功能";
+        public static LanguageElement LANG_COMMANDS_EXIT = "退出软件";
+        public static LanguageElement LANG_COMMANDS_CLEAR = "清空屏幕";
+        public static LanguageElement LANG_COMMANDS_STATUS = "获得当前连接状态属性";
+        public static LanguageElement LANG_COMMANDS_STOP = "停止当前连接";
+        public static LanguageElement LANG_COMMANDS_START = "开始同步";
+        public static LanguageElement LANG_COMMANDS_HELP = "打印帮助信息";
+        public static LanguageElement LANG_COMMANDS_SOURCEMSG = "danmaku <message> 发送弹幕测试";
+        public static LanguageElement LANG_COMMANDS_CLIENTMSG = "chat <message> 发送IRC信息测试";
+        public static LanguageElement LANG_COMMANDS_CLIENTUSERMSG = "chatuser <username> <message> 按照username名字发送IRC信息测试";
+        public static LanguageElement LANG_COMMANDS_SOURCES = "获得当前所有弹幕源列表";
+        public static LanguageElement LANG_COMMANDS_MSGMGR = "查看或者设置消息控制器相关内容,添加--help参数获取帮助";
+        public static LanguageElement LANG_COMMANDS_FILTERS = "列表所有当前可用消息过滤器";
+        public static LanguageElement LANG_COMMANDS_DISABLE = "向插件发送禁用消息 disable (插件名称)";
+        public static LanguageElement LANG_COMMANDS_SWITCH_CLIENT = "切换到指定Client实例，不带名称则为获取Client列表";
+        public static LanguageElement LANG_COMMANDS_SOURCELOGIN = "登录到弹幕源 sourcelogin [用户名] [密码]";
+        public static LanguageElement LANG_COMMANDS_RESTART = "重新启动应用程序";
+        public static LanguageElement LANG_COMMANDS_LANG = "lang [cultureName] Get/Set language";
+        public static LanguageElement LANG_COMMANDS_LISTLANG = "listlang [--all] List (supported/all) languages";
+        public static LanguageElement LANG_COMMANDS_FILTERS_ITEM = "过滤项";
+        public static LanguageElement LANG_COMMANDS_FILTERS_OBJ = "过滤器";
+        public static LanguageElement LANG_COMMANDS_CLIENT_NAME = "Client";
+        public static LanguageElement LANG_COMMANDS_CLIENT_AUTHOR = "作者";
+        public static LanguageElement LANG_COMMANDS_SOURCES_NAME = "弹幕源";
+        public static LanguageElement LANG_COMMANDS_SOURCES_AUTHOR = "作者";
+        public static LanguageElement LANG_COMMANDS_CURRENT = "当前设置为 {0:S}";
+        public static LanguageElement LANG_COMMANDS_DANMAKU_NOT_SUPPORT = @"提示：当前弹幕源不支持发送弹幕，请更换弹幕源！\n";
+        public static LanguageElement LANG_COMMANDS_CHAT_IRC_NOTCONNECT = "osu! irc 尚未连接，您还不能发送消息。";
+        public static LanguageElement LANG_COMMANDS_DANMAKU_REQUIRE_LOGIN = "你必须登录才能发送弹幕!";
+        public static LanguageElement LANG_COMMANDS_START_ALREADY_RUN = "同步实例已经在运行。";
+        public static LanguageElement LANG_COMMANDS_ARGUMENT_WRONG = "参数不正确";
+        public static LanguageElement LANG_COMMANDS_MSGMGR_HELP = @"\n--status :查看当前消息管理器的信息\n--limit <数值> :是设置限制发送信息的等级，越低就越容易触发管控\n--option <名称> :是设置管控的方式，其中auto是自动管控，force_all强行全都发送,force_limit是仅发送使用?send命令的消息";
+        public static LanguageElement LANG_COMMANDS_MSGMGR_LIMIT = "限制中...";
+        public static LanguageElement LANG_COMMANDS_MSGMGR_FREE = "无限制";
+        public static LanguageElement LANG_COMMANDS_MSGMGR_STATUS = "MessageManager mode:{4:S},status:{0:D},queueCount/limitCount/recoverTime:{1}/{2}/{3}";
+        public static LanguageElement LANG_COMMANDS_MSGMGR_LIMIT_SPEED_SET = "设置限制发送速度等级为{0}";
+        public static LanguageElement LANG_COMMANDS_MSGMGR_LIMIT_STYPE_SET = "设置消息管理器的管制方式为{0}";
+    }
 
     public interface I18nProvider
     {
@@ -114,15 +149,16 @@ namespace Sync.Tools
             return value;
         }
     }
+
     /// <summary>
     /// I18n Manager
     /// </summary>
     public class I18n
     {
         public static string CurrentSystemLang { get => System.Globalization.CultureInfo.CurrentCulture.Name; }
-        string Base { get => AppDomain.CurrentDomain.BaseDirectory; }
+        private string Base { get => AppDomain.CurrentDomain.BaseDirectory; }
         public string LangFolder { get => Path.Combine(Base, "Language"); }
-        public string SelectLangFolder {  get => Path.Combine(LangFolder, CurrentLanguage); }
+        public string SelectLangFolder { get => Path.Combine(LangFolder, CurrentLanguage); }
         public string CurrentLanguage;
 
         private static I18n instance;
@@ -135,7 +171,7 @@ namespace Sync.Tools
             {
                 if (instance == null)
                 {
-                    if(Configuration.Instance.Language == Configuration.DEFAULT_LANGUAGE || ((string)Configuration.Instance.Language).Length == 0)
+                    if (Configuration.Instance.Language == Configuration.DEFAULT_LANGUAGE || ((string)Configuration.Instance.Language).Length == 0)
                     {
                         instance = new I18n(CurrentSystemLang);
                     }
@@ -161,7 +197,9 @@ namespace Sync.Tools
             }
         }
 
-        private I18n() { }
+        private I18n()
+        {
+        }
 
         /// <summary>
         ///  Constructor for initial one language
@@ -180,7 +218,7 @@ namespace Sync.Tools
             string LangFile = Path.Combine(SelectLangFolder, instance.GetType().FullName) + ".lang";
             foreach (FieldInfo item in instance.GetType().GetFields())
             {
-                if(item.FieldType.Equals(typeof(LanguageElement)))
+                if (item.FieldType.Equals(typeof(LanguageElement)))
                 {
                     string value = ConfigurationIO.IniReadValue(LangFile, item.Name, CurrentLanguage);
                     if (value == "")
@@ -202,7 +240,5 @@ namespace Sync.Tools
                 }
             }
         }
-
-
     }
 }
