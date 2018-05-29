afe08602ef2150888381276d5c5be6db064c3bc7
Mikira Sora <mikirasora0409@126.com>
2018-05-29 15:55:10 +0800

    clean Program.cs

--- Sync/Program.cs
+++ Sync/Program.cs
@@ -38,8 +38,8 @@ namespace Sync
                 if (pid != 0)
                 {
                     CurrentIO.WriteColor(DefaultI18n.LANG_Instance_Exist, ConsoleColor.Red);
-                    bool wait = true;
-                    while (wait)
+
+                    while (true)
                     {
                         try
                         {
@@ -48,12 +48,14 @@ namespace Sync
                         }
                         catch (ArgumentException)
                         {
-                            wait = false;
+                            break;
                         }
                     }
                 }
+
                 byte[] pidBytes = BitConverter.GetBytes(Process.GetCurrentProcess().Id);
                 mmfs.Seek(0, SeekOrigin.Begin);
+
                 //write current sync.exe pid
                 mmfs.Write(pidBytes, 0, 4);
             }
@@ -61,35 +63,45 @@ namespace Sync
 
         static void Main(string[] args)
         {
-            if (Updater.ApplyUpdate(args)) return;
-            I18n.Instance.ApplyLanguage(new DefaultI18n());
-            SetConsoleCtrlHandler(cancelHandler, true);
-
-            using (var mmf = MemoryMappedFile.CreateOrOpen("{781d2da2-1b44-46d9-8b01-e1d59adc018b}",4))
+            using (var mmf = MemoryMappedFile.CreateOrOpen("{781d2da2-1b44-46d9-8b01-e1d59adc018b}", 4))
             {
                 //Check sync.exe is run
                 WaitLastSyncExit(mmf);
 
+                Init(args);
+                
                 while (true)
                 {
-                    SyncHost.Instance = new SyncHost();
-                    SyncHost.Instance.Load();
-
-                    CurrentIO.WriteWelcome();
-
-                    SyncHost.Instance.Plugins.ReadySync();
-
-                    if (Updater.IsUpdated) IO.CurrentIO.WriteColor("Sync is already up to date!", ConsoleColor.Green);
-
-                    string cmd = CurrentIO.ReadCommand();
-                    while (true)
-                    {
-                        SyncHost.Instance.Commands.invokeCmdString(cmd);
-                        cmd = CurrentIO.ReadCommand();
-                    }
+                    var cmd = CurrentIO.ReadCommand();  
+                    SyncHost.Instance.Commands.invokeCmdString(cmd);
                 }
             }
         }
 
+        static void Init(string[] args)
+        {
+            //检查更新
+            if (Updater.ApplyUpdate(args))
+                return;
+
+            //初始化I18n
+            I18n.Instance.ApplyLanguage(new DefaultI18n());
+
+            //添加控制台窗口关闭事件捕捉
+            SetConsoleCtrlHandler(cancelHandler, true);
+
+            //初始化Sync核心
+            SyncHost.Instance = new SyncHost();
+            SyncHost.Instance.Load();
+
+            CurrentIO.WriteWelcome();
+
+            SyncHost.Instance.Plugins.ReadySync();
+
+            //Sync本体更新检查
+            if (Updater.IsUpdated)
+                IO.CurrentIO.WriteColor("Sync is already up to date!", ConsoleColor.Green);
+
+        }
     }
 }
