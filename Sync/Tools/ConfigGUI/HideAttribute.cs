818c78e0e906f7e1ea0a24a06922fcd59b755392
KedamaOvO <moe@mao-yu.net>
2018-06-14 17:38:19 +0800

    add HideAttribute

---
+++ Sync/Tools/ConfigGUI/HideAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Sync.Tools.ConfigGUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sync.Tools.ConfigGUI
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class HideAttribute : Attribute
+    {
+    }
+}
