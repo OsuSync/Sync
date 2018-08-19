f3c57467216c81c8922a30aef4e8618efa38f2a6
Mikira Sora <mikirasora0409@126.com>
2018-08-19 23:49:22 +0800

    fixed SaveSync() crash

--- Sync/SyncHost.cs
+++ Sync/SyncHost.cs
@@ -147,8 +147,8 @@ namespace Sync
 
         public void SaveSync()
         {
-            if (ClientWrapper.Client != null) ClientWrapper.Client?.StopWork();
-            if (SourceWrapper.Source != null) SourceWrapper.Source?.Disconnect();
+            ClientWrapper?.Client?.StopWork();
+            SourceWrapper?.Source?.Disconnect();
 
             foreach (var item in PluginConfigurationManager.ConfigurationSet)
             {
